2d694b5ad75af08c0fc54f131a53df34a4c37b4b
Hara <hara.acharya@gmail.com>
2020-02-09 11:50:12 -0800

    added useState hook for signup

--- dealregistration-ui/src/user/Signup.js
+++ dealregistration-ui/src/user/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../core/Layout";
 
 const Signup = () => {
@@ -10,25 +10,27 @@ const Signup = () => {
         error: '',
         success: false
     })
-    
+    const handleChange = name => event => {
+        setValues({...values, error:false, [name]: event.target.value})            
+    }
     const SignUpFrom = () => (
        <div className="container col-md-8 offset-md-2">
             <form>
                 <div className="form-group">
                     <label className="text-muted">UserType</label>
-                    <input type="text" className="form-control" />
+                    <input onChange={handleChange('usertype')} type="text" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">Name</label>
-                    <input type="text" className="form-control" />
+                    <input onchange={handleChange('name')} type="text" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">Email</label>
-                    <input type="email" className="form-control" />
+                    <input onchange={handleChange('email')} type="email" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">Password</label>
-                    <input type="password" className="form-control" />
+                    <input onChange={handleChange('password')} type="password" className="form-control" />
                 </div>
                 <button className="btn btn-primary">Submit</button>
             </form>
@@ -37,6 +39,7 @@ const Signup = () => {
     return (
         <Layout title="Signup" description="Signup to deal registration site">
             {SignUpFrom()}
+            {JSON.stringify(values)}
         </Layout>
     );
 };
